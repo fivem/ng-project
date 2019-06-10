9a0afc705122d141ce275984f79b3f4116f2ed8c
fivem <forever113@126.com>
2019-06-10 20:35:18 +0800

    embed bpmn-js

--- ng-bpmn-modeler/src/app/app-routing.module.ts
+++ ng-bpmn-modeler/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {BpmnModelerComponent} from './bpmn-modeler/bpmn-modeler.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'bpmn',
+    component: BpmnModelerComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- ng-bpmn-modeler/src/app/app.module.ts
+++ ng-bpmn-modeler/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BpmnModelerComponent } from './bpmn-modeler/bpmn-modeler.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BpmnModelerComponent
   ],
   imports: [
     BrowserModule,
