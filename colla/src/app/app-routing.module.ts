15595574f4fa56a3ded80ebc04823db283ef273d
fivem <forever113@126.com>
2019-06-12 18:06:12 +0800

    router lazy-load with bug

--- colla/src/app/app-routing.module.ts
+++ colla/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './login/login.component';
 
 const routes: Routes = [
   {path: ``, redirectTo: `/login`, pathMatch: `full`},
-  {path: `login`, loadChildren: './login/login.module#LoginModule' },
-
+  /*lazy-load fail*/
+  /*{path: `login`, loadChildren: './login/login.module#LoginModule'},*/
+  {path: ``, redirectTo: `/login`, pathMatch: `full`},
+  {path: `login`, component: LoginComponent}
 ];
 
 @NgModule({
--- colla/src/app/login/login.module.ts
+++ colla/src/app/login/login.module.ts
@@ -1,19 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {LoginComponent} from './login.component';
-import { Routes, RouterModule} from '@angular/router';
 
-const routes: Routes = [
-  {
-    path: 'login',
-    component: LoginComponent
-  }
-];
 @NgModule({
   declarations: [LoginComponent],
   imports: [
-    CommonModule,
-    RouterModule.forChild(routes)
+    CommonModule
   ]
 })
 export class LoginModule { }
